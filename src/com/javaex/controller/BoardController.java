4e45aa1fe249d2d95a9299cbd56814167c8ca400
whjkh <whjkh@DESKTOP-O9T8HTK>
2021-01-15 17:26:35 +0900

    일단 컨트롤러만 리부트

--- src/com/javaex/controller/BoardController.java
+++ src/com/javaex/controller/BoardController.java
@@ -30,59 +30,18 @@ public class BoardController extends HttpServlet {
 		if ("list".equals(act)) {
 			System.out.println(act + " 게시판메인");
 
-			// list load
-			BoardDao bDao = new BoardDao();
-			List<BoardVo> bList = bDao.boardList();
-
-			// list data attribute to jsp
-			request.setAttribute("boardMain", bList);
-
-			// list forword
-			WebUtil.forword(request, response, "/WEB-INF/views/board/list.jsp");
 		} else if ("writeForm".equals(act)) {
 			System.out.println(act + "게시글 작성 창");
 
-			// read forword
-			WebUtil.forword(request, response, "/WEB-INF/views/board/writeForm.jsp");
 		} else if ("write".equals(act)) {
 			System.out.println(act + "게시글 작성");
 
-			HttpSession session = request.getSession();
-			UserVo authUser = (UserVo) session.getAttribute("authUser");
-
-			int no = authUser.getNo();
-			String title = request.getParameter("title");
-			String content = request.getParameter("content");
-
-			BoardVo bVo = new BoardVo(no, title, content);
-
-			System.out.println(bVo.toString());
-
-			BoardDao bDao = new BoardDao();
-			bDao.insert(bVo);
-
-			WebUtil.redirect(request, response, "/mysite2/board?action=list");
 		} else if ("read".equals(act)) {
 			System.out.println(act + "게시글 보기");
 
-			int no = Integer.parseInt(request.getParameter("no"));
-
-			BoardDao bDao = new BoardDao();
-			BoardVo bVo = bDao.readList(no);
-
-			request.setAttribute("readList", bVo);
-
-			// read forword
-			WebUtil.forword(request, response, "/WEB-INF/views/board/read.jsp");
 		} else if ("delete".equals(act)) {
 			System.out.println(act + "게시글 삭제");
 
-			int no = Integer.parseInt(request.getParameter("no"));
-
-			BoardDao bDao = new BoardDao();
-			bDao.delete(no);
-
-			WebUtil.redirect(request, response, "/mysite2/board?action=list");
 		}
 	}
 
